de597e847d43a9af9a7ca5aca8ec90b324bd0749
sanjarbek <sanjarsaidov173@gmail.com>
2023-03-29 23:42:18 +0500

    database is done

--- db.py
+++ db.py
@@ -20,7 +20,76 @@ class Database:
 
         self.group.insert({'group_id': l, 'user_ids': [user_id]})
 
+    def add_user_to_group(self, user_id, group_id):
+        '''add user to group'''
+        self.group.update({'user_ids': Query().user_ids.append(user_id)}, Query().group_id == group_id)
+
+    def add_queue_all_group(self):
+        '''add queue to all groups'''
+        for i in self.group.all():
+            self.queue.insert({'group_id': i['group_id'], 'day_count': 0})
+
+    def add_date(self, date, is_done, group_id):
+        '''add date to corresponding group that is queue is done or not'''
+        self.date.insert({'date': date, 'is_done': is_done, 'group_id': group_id})
+
+        # if is_done true remove that group from queue
+        if is_done:
+            self.queue.remove(Query().group_id == group_id)
+
+    def add_admin(self, user_id, password):
+        '''add admin to database'''
+        # add admin if password equals to 1717
+        if password == '1717':
+            self.admin.insert({'user_id': user_id})
+
+    def get_user(self, user_id):
+        '''get user from database'''
+        return self.user.get(Query().user_id == user_id)
     
-
-
+    def get_group(self, group_id):
+        '''get group from database'''
+        return self.group.get(Query().group_id == group_id)
+    
+    def get_queue(self, group_id):
+        '''get queue from database'''
+        return self.queue.get(Query().group_id == group_id)
+    
+    def get_date(self, date):
+        '''get date from database'''
+        return self.date.get(Query().date == date)
+    
+    def check_admin(self, user_id):
+        '''check if user is admin'''
+        return self.admin.get(Query().user_id == user_id)
+    
+    def get_all_users(self):
+        '''get all users from database'''
+        return self.user.all()
+    
+    def get_all_groups(self):
+        '''get all groups from database'''
+        return self.group.all()
+    
+    def get_all_queues(self):
+        '''get all queues from database'''
+        return self.queue.all()
+    
+    def get_all_dates(self):
+        '''get all dates from database'''
+        return self.date.all()
+    
+    def get_all_admins(self):
+        '''get all admins from database'''
+        return self.admin.all()
+    
+    def get_all_users_from_group(self, group_id):
+        '''get all users from group'''
+        return self.group.get(Query().group_id == group_id)['user_ids']
+    
+    def get_all_dates_from_group(self, group_id):
+        '''get all dates from group'''
+        return self.date.search(Query().group_id == group_id)
+    
+    
     
